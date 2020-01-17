011a18a6c123151df4ccf8b13029339d2f35f6fb
waqqasiq <waqqas.ess@gmail.com>
2020-01-17 18:44:04 +0600

    dialog box to select folder added

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # User Defined File/Folder Organizer
-A smart file-organizer to move files of similar type to their designated folder. 
+A smart file-organizer to move files of similar type to their designated folder
 
-- Put the file extensions and folder names you want in **fileFolder.csv**
-- Run the **groupFiles.py** and wait a few seconds
-- You may check the **logfile.txt** to know the whereabouts of the folders and files
+- Put file extensions and folder names you want in **fileFolder.csv**
+- Run **groupFiles.py**
 
-**Note**: Keep the **fileFolder.csv** and **groupFiles.py** in the same folder as the other files when running.
+**Note**: Keep the **fileFolder.csv** and **groupFiles.py** in the same folder
 
 
 
--- groupFiles.py
+++ groupFiles.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 31 14:15:06 2019
+Created on Fri Jan 17 16:28:02 2020
 
-@author: waqqas.iqbal
+@author: Waqqas
 """
+
 import os
 import shutil
 import pandas as pd
+from tkinter import filedialog
+from tkinter import *
 
-path = os.getcwd()
-names = os.listdir(path)
+#path = os.getcwd()
+#names = os.listdir(path)
 
 df = pd.read_csv('fileFolder.csv')
 
@@ -23,36 +26,26 @@ for f in df['FileName']:
 
 folder_name_set = list(set(folder_name))   
 
-######## CURRENT TIME #################
-from datetime import datetime
-from pytz import timezone
-def date_time(zone='Asia/Dhaka'):
-    other_zone = timezone(zone)
-    other_zone_time = datetime.now(other_zone)
-    year = other_zone_time.strftime('%y')
-    month = other_zone_time.strftime('%m')
-    day = other_zone_time.strftime('%d')
-    hour = other_zone_time.strftime('%H')
-    minute = other_zone_time.strftime('%M')
-    if int(hour)>12:
-        hour = int(hour) - 12
-        minute = str(minute)+"pm"
-    else:
-        minute = str(minute)+"am"
-    return str(day)+"-"+str(month)+"-"+str(year)+"-"+str(hour)+"-"+str(minute)
-######################################
-    
-date_time = date_time()  
 
-file = open("logfile.txt","w") 
+root = Tk()
+root.withdraw()
+folder_selected = filedialog.askdirectory()
+
+#print("folder selected")
+#print(folder_selected)
+
+names = os.listdir(folder_selected)
+#print("names")
+#print(names)
+
 folder_created = []
 files_moved = []
-
-j=0 #Count of files moved    
 k=0 #count of folder created 
+j=0 #Count of files moved
+
 
 for files in names:
-    if ((files=="fileFolder.csv") | (files=="groupFiles.py") | (files=="logfile.txt")):
+    if ((files=="fileFolder.csv") | (files=="groupFiles.py")):
         continue
     iterator = -1
     c = 0
@@ -65,31 +58,16 @@ for files in names:
 
     for i in range(len(file_name)):
         if file_extension==file_name[i]:
-            if not os.path.exists(path+"\\"+folder_name[i]):
-                os.makedirs(path+"\\"+folder_name[i])
+            if not os.path.exists(folder_selected+"\\"+folder_name[i]):
+                os.makedirs(folder_selected+"\\"+folder_name[i])
                 folder_created.append(folder_name[i])
                 k+=1
-            shutil.move(path+"\\"+files, path+"\\"+folder_name[i]+"\\"+files)
+            shutil.move(folder_selected+"\\"+files, folder_selected+"\\"+folder_name[i]+"\\"+files)
             files_moved.append(files)
             j+=1
-    
-print("Folder created: ", k)   
-print("Files moved: ", j)
-print("Check the logfile.txt for more details.")
 
-file.write("Folder created: \n")
-for f in folder_created:
-    file.write(f + "\n")
+print("Folder created:")
+print(k)
 
-file.write("\n")
-
-file.write("Files moved: \n")
-for f in files_moved:
-    file.write(f + "\n")
-
-file.write("\n") 
-file.write("Folder created: "+ str(k) + "\n")  
-file.write("Files moved: "+ str(j) + "\n")   
-file.write("Timstamp (d-m-y-H-M) : "+date_time)  
- 
-file.close() 
+print("Files moved:")
+print(j)
